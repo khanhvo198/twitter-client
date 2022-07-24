cd25170f2a5b34d158887a1648362c69a986bb26
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-07-24 22:30:35 +0700

    init project

---
+++ src/api/axiosClient.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from "axios";
+import queryString from "query-string";
+
+const baseURL : string = "http:localhost:8080/api/v1";
+
+function getToken() : string {
+  return "Token DMM"
+}
+
+const axiosClient : AxiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "content-type": 'application/json'
+  },
+  paramsSerializer: (param) => queryString.stringify(param)
+})
+
+
+axiosClient.interceptors.request.use(async (config) => {
+  const token : string = getToken()
+  config.headers!.Authorization = `Bearer ${token}`
+})
+axiosClient.interceptors.response.use((res) => {
+  if(res && res.data) {
+    return res.data
+  }
+  return res
+}, (err) => {
+  throw err
+})
+
+export default axiosClient
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactElement } from "react";
+import { Button } from "reactstrap";
+
+const LoginPage: FC = (): ReactElement => {
+  return(
+    <div>
+      <Button color="danger">Danger</Button>
+    </div>
+  )
+} 
+export default LoginPage
