f7f4ba78a95da6d09a9f2a556a4cad72fdfccd96
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-09-08 22:44:25 +0700

    refactor home

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React, { FC, ReactElement } from 'react';
+import { Tweet } from '../Tweet/Tweet';
 import { TweetBox } from '../TweetBox/TweetBox';
+import { TopTweetIcon } from './components/TopTweetIcon/TopTweetIcon';
 import { useHomeStyle } from './style';
 
 export const Home: FC = (): ReactElement => {
@@ -8,7 +10,49 @@ export const Home: FC = (): ReactElement => {
   return (
     <React.Fragment>
       <div className={classes.container}>
+        <div className={classes.tweetBoxTitleContainer}>
+          <div className={classes.tweetBoxFirstElementContainer}>
+            <h5>Home</h5>
+          </div>
+          <div className={classes.tweetBoxSecondElementContainer}>
+            <div>
+              <TopTweetIcon />
+            </div>
+          </div>
+        </div>
         <TweetBox />
+        <Tweet />
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
+
+        <Tweet />
       </div>
     </React.Fragment>
   );
--- src/features/Home/style.ts
+++ src/features/Home/style.ts
@@ -4,6 +4,49 @@ import { Theme } from '@mui/system';
 export const useHomeStyle = makeStyles((theme: Theme) => ({
   container: {
     backgroundColor: '#000',
-    height: '100vh',
+    color: 'rgb(239, 243, 244)',
+    borderLeft: '1px solid rgb(47,51,54)',
+    borderRight: '1px solid rgb(47,51,54)',
+  },
+  tweetBoxTitleContainer: {
+    minHeight: '53px',
+    padding: '0 1rem 0 1rem',
+    display: 'flex',
+    width: '100%',
+    opacity: 0.8,
+    position: 'sticky',
+    top: 0,
+    zIndex: 100,
+    backgroundColor: 'rgba(0, 0, 0, 0.65)',
+    backdropFilter: 'blur(12px)',
+  },
+  tweetBoxFirstElementContainer: {
+    width: '90%',
+    display: 'flex',
+    alignItems: 'center',
+    '& h5': {
+      margin: 0,
+      fontWeight: 'bold',
+    },
+  },
+  tweetBoxSecondElementContainer: {
+    width: '10%',
+    color: 'rgb(239, 243, 244)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    '& div': {
+      minWidth: '36px',
+      minHeight: '36px',
+      marginRight: '-9px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    '& div:hover': {
+      cursor: 'pointer',
+      borderRadius: '50%',
+      backgroundColor: 'rgb(24,24,24)',
+    },
   },
 }));
