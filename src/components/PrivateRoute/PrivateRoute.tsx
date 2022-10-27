8d4fe9f73809ce4ade37e45226a14c57b3b58253
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-10-27 22:43:30 +0700

    make private route

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,8 +1,20 @@
 import { FC, ReactElement } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+
+export interface tokenObject {
+  exp: Number;
+}
 
 export const PrivateRoute: FC = (): ReactElement => {
   const isLoggedIn = localStorage.getItem('token');
-  if (!isLoggedIn) return <Navigate to="/login" replace />;
+  console.log(isLoggedIn);
+  const jwtDecode: tokenObject = jwt_decode(isLoggedIn!);
+  console.log(jwtDecode);
+  if (jwtDecode.exp < Date.now() / 1000) {
+    localStorage.clear();
+    return <Navigate to="/login" replace />;
+  }
+
   return <Outlet />;
 };
